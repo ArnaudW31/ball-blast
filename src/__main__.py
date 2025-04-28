619cf9935fbb9a5599091227a3e41938249e7a60
WISSOCQ Arnaud <arnaudwq@gmail.com>
2025-04-28 14:24:01 +0200

    feat:death animation
    explosion quand on touche une boule

--- src/__main__.py
+++ src/__main__.py
@@ -13,12 +13,17 @@ pygame.font.init()
 Font = pygame.font.SysFont('Comic Sans MS', 30)
 
 # Set up the display
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen : pygame.Surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Ball Blast")
-texture = pygame.image.load('./assets/bg.jpg')
 
-ball_level = [[RED,30],[GREEN,25],[BLUE,20]]
-perdu = False
+# Création des variables de jeu
+ball_level = [[BLACK,40],[WHITE,35],[RED,30],[GREEN,25],[BLUE,20]]
+perdu : bool = False
+shootCD : int = 0
+texture : pygame.Surface = pygame.image.load('./assets/bg.jpg')
+
+frameNumber : int = 0
+path : str = "./assets/explosion_frames/frame-"
 
 # Initialize player, balls, and bullets
 player = Player()
@@ -32,8 +37,9 @@ all_sprites.add(playerGroup)
 all_sprites.add(wheels[0])
 all_sprites.add(wheels[1])
 
-for _ in range(8):
-    newball = Ball(random.randint(0, SCREEN_WIDTH - 30),random.randint(-100, -40),30)
+# Création des boules
+for _ in range(2):
+    newball = Ball(random.randint(0, SCREEN_WIDTH),random.randint(-100, -40),ball_level[0][1],0,ball_level[0][0])
     balls.add(newball)
     all_sprites.add(newball)
 
@@ -46,11 +52,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                bullet = Bullet(player.rect.centerx, player.rect.top)
-                all_sprites.add(bullet)
-                bullets.add(bullet)
+    
+    
+    shootCD += 1
+    
+    #Toutes les 10 frames, on tire
+    if shootCD == 10 and not perdu:
+        shootCD = 0
+        bullet = Bullet(player.rect.centerx, player.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
 
     # Update
     all_sprites.update()
@@ -58,31 +69,61 @@ while running:
     # Check for collisions
     hitBalls = pygame.sprite.groupcollide(balls, bullets, True, True)
     for hit in hitBalls:
+        #Si la boule touchée n'est pas la plus petite
         if hit.level < len(ball_level)-1:
+            
+            #On crée une boule aux mêmes co que la boule détruite
             ball1 = Ball(hit.rect.x,-40,ball_level[hit.level+1][1],hit.level+1,ball_level[hit.level+1][0])
+            
+            #on la décale à droite
             ball1.decale(50)
+            
+            #Ajout au groupe de collision des boules
             balls.add(ball1)
+            
+            #Ajout au groupe de tout les sprites
             all_sprites.add(ball1)
             
-            ball2 = Ball(hit.rect.x,-40,ball_level[hit.level+1][1],hit.level+1,ball_level[hit.level+1][0])
-            ball2.decale(-50)
-            balls.add(ball2)
-            all_sprites.add(ball2)
             
-    hitPlayer = pygame.sprite.groupcollide(balls, playerGroup, False, False)
+            #On crée une autre boule
+            ball2 = Ball(hit.rect.x,-40,ball_level[hit.level+1][1],hit.level+1,ball_level[hit.level+1][0])
+            
+            #On la décale à gauche
+            ball2.decale(-50)
+            
+            #Ajout au groupe de collision des boules
+            balls.add(ball2)
+            
+            #Ajout au groupe de tout les sprites
+            all_sprites.add(ball2)
 
     # Draw / render
     
+    # On render le fond en premier sinon tout est derrière
     screen.blit(texture, (0,0))
+    
     all_sprites.draw(screen)
     
+    hitPlayer = pygame.sprite.groupcollide(balls, playerGroup, False, False)
+    
+    #Si le perso a été touché par une balle, il perd
     if len(hitPlayer) > 0:
         perdu = True
+        player.kill()
         
     if perdu:
         text_surface = Font.render('PERDUUUUUUU', False, (0, 0, 0))
         screen.blit(text_surface,(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
         
+        if frameNumber < 17:
+            frameNumber += 1
+            if frameNumber <= 9 :
+                deathImage = pygame.image.load(path + "0" + str(frameNumber) + ".png")
+            else:
+                deathImage = pygame.image.load(path + str(frameNumber) + ".png")
+            screen.blit(deathImage,(player.rect.left-20,player.rect.top-80))
+    
+    #Si il n'y a plus aucune balle, il gagne
     if len(balls.sprites()) == 0:
         text_surface = Font.render('GAGNÉ', False, (0, 0, 0))
         screen.blit(text_surface,(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
